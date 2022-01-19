496d137ed21f2b3a077adaab7e312ea1185e8123
RaturiAR7 <98044125+RaturiAR7@users.noreply.github.com>
2022-01-19 22:42:53 +0530

    Day 2 assignmet submission

---
+++ day2.py
@@ -0,0 +1,7 @@
+Age=19;
+City="Dehradun";
+Name="Anshul";
+percentage=89.5;
+print("Hello,Team let's upgrade");
+print("My name is %s and I am %s years old.I live in %s"%(Name,Age,City));
+print(f"My 12th percentage={percentage}");
