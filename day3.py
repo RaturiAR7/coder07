421f59124db0e24c5a69a689d3c48cc636af0e2b
RaturiAR7 <98044125+RaturiAR7@users.noreply.github.com>
2022-01-20 22:01:07 +0530

    Days 3-Assignment

---
+++ day3.py
@@ -0,0 +1,5 @@
+#Assignment-Use list to print detials of the user
+totalInput=list()
+for i in range(5):
+    totalInput.append(input("Enter name,age,city,profession and qualification"))
+    print(f"Informatio:{totalInput(i)}")
