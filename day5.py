57b88ef97a131cd7d4f29d315f34c94cabd571a7
RaturiAR7 <98044125+RaturiAR7@users.noreply.github.com>
2022-01-22 21:15:48 +0530

    day 5-assignment

---
+++ day5.py
@@ -0,0 +1,11 @@
+#program to convince you that anshul must get certificate
+print(f"Sorry sir/ma'am .I wasn't able to submit the django\
+assignment.Actually I did not understand what we were supposed\
+    to do in the fifth assignment.\
+        Please enter a number you would like to give me out of 10 for this program")
+        marks =input("Please enter a number:")
+        print(marks)
+        if(marks>3)
+        print("I deserve certificate")
+        else
+        print("please sir I really want this certificate")
